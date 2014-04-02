6ed35722b6d651382eaf67a9ef4a3d042bc8ea41
Elias Martinez <elias.martinez@amplemind.com>
2014-04-02 17:15:25 -0600

    [initial state]

--- rubywarrior/1-beginner/player.rb
+++ rubywarrior/1-beginner/player.rb
@@ -1,56 +1,44 @@
 class Player
   @needs_rest = false
   @health = nil
-  @@minimum_required_health = 14
-  @@captives = 0
-  @@direction = :forward
+  @@minimum_required_turns = 2
+  
   def play_turn(warrior)
     # unless next cell empty
-    being_attacked = false
+    surroundings = warrior.look
+
     unless warrior.feel.empty?
-      
       # if its a enemy
       if warrior.feel.enemy?
         # evaluate if no capptives
-        if !hear_captives? #and !@needs_rest # no captives
           warrior.attack!
-        else #lets rescue
-          warrior.walk! :backward
-        end
-      
       elsif warrior.feel.captive?
-        @@captives = @@captives - 1
         warrior.rescue! 
-      
       elsif warrior.feel.wall?
         warrior.pivot!
-      else
-        lets_do_it if !hear_captives?
-        warrior.walk! 
+      else        
+        warrior.walk!
       end
       @needs_rest = needs_rest?(warrior)
     else 
+      if !surroundings[0].captive? && (surroundings[1].enemy? || surroundings[2].enemy? ) 
+        warrior.shoot! 
       # if being attacked
-      if @health and @health > warrior.health 
-        being_attacked = true
+      elsif @health and @health > warrior.health 
         unless @needs_rest
           warrior.walk! 
         else
           warrior.walk! :backward
         end
         @needs_rest = needs_rest?(warrior)
-      elsif @needs_rest # if needs rest and not being attacked
-        
+
+      # if needs rest and not being attacked 
+      elsif @needs_rest 
         warrior.rest!
-        
         @needs_rest = needs_rest?(warrior)
+      # else walk
       else
-        if hear_captives?
-          warrior.pivot
-        else
-          warrior.walk!
-        end 
-        
+        warrior.walk!
       end
     end
     # set instance of current heald
@@ -59,24 +47,7 @@ class Player
 
   def needs_rest?(warrior)
     # if warrior resist at least 2 turns more
-    (warrior.health / 3) <= 2 ? true : false
+    (warrior.health / 3) <= @@minimum_required_turns ? true : false
   end
-
-  def switch_direction
-    @@direction = @@direction == :forward ? :backward : :forward
-  end
-
-  def lets_do_it
-    @@direction = :forward
-  end
-
-  def oh_fuck
-    @@direction = :backward
-  end
-
-  def hear_captives?
-    @@captives > 0
-  end
-
   
 end
